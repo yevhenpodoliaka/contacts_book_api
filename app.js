fdb38b3161bb42871566e48a51e6e9756605da7b
Yevhen Podoliaka <zanp85740@gmail.com>
2022-11-01 09:48:09 +0200

    fixed static path

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
-app.get("/", (req, res) => {
+
 
 app.use("/api/auth", authRouter);
 app.use("/api/contacts", contactsRouter);
