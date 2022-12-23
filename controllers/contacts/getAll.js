73c93ec75db59f31c844122a0df7c03303faa8a5
Yevhen Podoliaka <zanp85740@gmail.com>
2022-12-23 06:35:22 +0200

    fixed getAll contacts

--- controllers/contacts/getAll.js
+++ controllers/contacts/getAll.js
@@ -4,28 +4,20 @@ const getAll = async (req, res) => {
   const { _id } = req.user;
   const { page = 1, limit = 20, favorite } = req.query;
   const skip = (page - 1) * limit;
-  if (favorite) {
-    const contacts = await Contact.find({ owner: _id, favorite }, "", {
-      skip,
-      limit: Number(limit),
-    }).populate("owner", "name email");
-    res.json({
+
+   const contacts = await Contact.find(
+     favorite ? { owner:_id, favorite } : { owner:_id },
+     "",
+     { skip, limit }
+  ).populate("owner", "name email");
+  res.json({
       status: "success",
       code: 200,
       data: {
         result: contacts,
       },
     });
-  }
-  const contacts = await Contact.find({ owner: _id }, "", {
-    skip,
-    limit: Number(limit),
-  }).populate("owner", "name email");
-  res.json({
-    status: "success",
-    code: 200,
-    contacts,
-  });
+
 };
 
 module.exports = getAll;
