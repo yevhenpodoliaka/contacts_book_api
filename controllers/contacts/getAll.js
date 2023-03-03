d4cb388d1e055fab18190c8c8607bbd849417faa
Yevhen Podoliaka <zanp85740@gmail.com>
2023-03-03 09:50:15 +0200

    chenge default limit on page

--- controllers/contacts/getAll.js
+++ controllers/contacts/getAll.js
@@ -2,7 +2,7 @@ const { Contact } = require("../../models");
 
 const getAll = async (req, res) => {
   const { _id } = req.user;
-  const { page = 1, limit = 20, favorite } = req.query;
+  const { page = 1, limit = 100, favorite } = req.query;
   const skip = (page - 1) * limit;
 
    const contacts = await Contact.find(
